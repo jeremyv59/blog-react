dc178451b34e564a1c8e1fffcd45de5552eb4961
jérémy vaneste <jeremy.vaneste@free.fr>
2022-04-29 16:13:52 +0200

    correction design blog card

--- src/components/post-item/PostItem.jsx
+++ src/components/post-item/PostItem.jsx
@@ -5,7 +5,7 @@ import "../post-item/post-item.css";
 const PostItem = ({ title }) => {
   return (
     <Card className="card_container">
-      <p className="card_title">{`${title.substring(0, 150)}...`}</p>
+      <h3 className="card_title">{`${title.substring(0, 50)}...`}</h3>
       <a className="article_link">Lire l'article</a>
     </Card>
   );
--- src/components/posts-list/PostsList.jsx
+++ src/components/posts-list/PostsList.jsx
@@ -1,50 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PostItem from "../post-item/PostItem";
 import "../posts-list/postslist.css";
+import axios from "axios";
 
 const PostsList = () => {
-  const [postsList, setPostsList] = useState([
-    {
-      title:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem unde corporis dicta ratione quae excepturi rem ullam ea, consequuntur vero expedita rerum libero ad quas voluptates, neque praesentium perferendis maiores.",
-    },
-    {
-      title:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit minus autem sit iure ullam tenetur, temporibus omnis incidunt aspernatur provident quo nostrum ex obcaecati dolores necessitatibus",
-    },
-    {
-      title:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit minus autem sit iure ullam tenetur, temporibus omnis incidunt aspernatur provident quo nostrum ex obcaecati dolores necessitatibus",
-    },
-    {
-      title:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit minus autem sit iure ullam tenetur, temporibus omnis incidunt aspernatur provident quo nostrum ex obcaecati dolores necessitatibus",
-    },
-    {
-      title:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit minus autem sit iure ullam tenetur, temporibus omnis incidunt aspernatur provident quo nostrum ex obcaecati dolores necessitatibus",
-    },
-    {
-      title:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit minus autem sit iure ullam tenetur, temporibus omnis incidunt aspernatur provident quo nostrum ex obcaecati dolores necessitatibus",
-    },
-    {
-      title:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit minus autem sit iure ullam tenetur, temporibus omnis incidunt aspernatur provident quo nostrum ex obcaecati dolores necessitatibus",
-    },
-    {
-      title:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit minus autem sit iure ullam tenetur, temporibus omnis incidunt aspernatur provident quo nostrum ex obcaecati dolores necessitatibus",
-    },
-  ]);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const getData = async () => {
+    try {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/posts?_limit=25`
+      );
+      setData(response.data);
+      setError(null);
+    } catch (err) {
+      setError(err.message);
+      setData(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <div className="container">
       <h1 className="title">Tous les articles</h1>
       <div className="container_posts">
-        {postsList.map((post) => {
-          return <PostItem title={post.title}></PostItem>;
-        })}
+        {data ? (
+          data.map((post) => {
+            return <PostItem key={post.id} title={post.title}></PostItem>;
+          })
+        ) : (
+          <p>loading</p>
+        )}
       </div>
     </div>
   );
